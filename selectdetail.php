c8fcdcaae3bcce298562b92da10d30258498f015
unknown <sawanon01@hotmail.com>
2019-05-15 14:49:45 +0700

    update #15/05/2019

--- selectdetail.php
+++ selectdetail.php
@@ -1,15 +1,35 @@
 <?php
 include("connect.php");
 $strsql = "SELECT *
-FROM booking
-WHERE b_id = '".$_GET['a']."'";
+FROM booking,user
+WHERE b_id = '".$_GET['a']."'
+AND driver_id = user.u_id";
 $query = $conn->query($strsql);
 while ($result = $query->fetch_array()) {
   $b_id = $result['b_id'];
   $start = $result['b_startdatetime'];
   $end = $result['b_enddatetime'];
   $detail = $result['b_detail'];
-
+  $driver = $result['u_fname'];
+  $driver .= " ".$result['u_lname'];
+  $applicant = $result['applicant_id'];
+  $c_id = $result['c_id'];
+}
+$strsql = "SELECT * FROM user WHERE u_id = '".$applicant."'";
+$query = $conn->query($strsql);
+while ($result = $query->fetch_array()) {
+  $applicant = $result['u_fname'];
+  $applicant .= " ".$result['u_lname'];
+  $tel = $result['u_tel'];
+}
+$strsql = "SELECT * FROM car WHERE c_id = '".$c_id."'";
+$query = $conn->query($strsql);
+while ($result = $query->fetch_array()) {
+  $c_id = $result['c_id'];
+  $c_model = $result['c_model'];
+  $c_brand = $result['c_brand'];
+  $c_fuel = $result['c_fuel'];
+  $c_license = $result['c_licenseplate'];
 }
  ?>
 <!DOCTYPE html>
@@ -31,28 +51,34 @@ while ($result = $query->fetch_array()) {
     <div class="row">
       <div class="col-sm-4">
         <div style="margin-bottom: 10px;">
-          <img src="img/car/1.jpg" width="100%">
+          <img src="img/car/<?php echo $c_id; ?>.jpg" width="100%">
         </div>
       </div>
       <div class="col-sm-8">
-        Mazda RX-8
+        <?php
+        echo "ป้านทะเบียน : ".$c_license;
+        echo "<br />";
+        echo "ยี่ห้อ/รุ่น : ".$c_brand." ".$c_model;
+        echo "<br />";
+        echo "น้ำมัน : ".$c_fuel;
+        ?>
       </div>
     </div>
     <table id="table" class="table table-bordered table-hover">
       <tr>
-        <td class="header">เลขที่ใบจอง</td><td colspan="3">154</td>
+        <td class="header">เลขที่ใบจอง</td><td colspan="3"><?php echo $b_id; ?></td>
       </tr>
       <tr>
-        <td class="header">วันเวลาที่เริ่มใช้</td><td>2019-05-14 15:30</td><td class="header">วันเวลาที่นำมาคืน</td><td>2019-05-15 16:00</td>
+        <td class="header">วันเวลาที่เริ่มใช้</td><td><?php echo $start; ?></td><td class="header">วันเวลาที่นำมาคืน</td><td><?php echo $end; ?></td>
       </tr>
       <tr>
-        <td class="header">รายละเอียด</td><td colspan="3"> ระยอง </td>
+        <td class="header">รายละเอียด</td><td colspan="3"><?php echo $detail; ?></td>
       </tr>
       <tr>
-        <td class="header">ใช้สำหรับแผนก</td><td>ฝ่ายบริหารทั่วไป</td><td class="header">พนักงานขับรถ</td><td>นาย ขอขับ แปป</td>
+        <td class="header">ใช้สำหรับแผนก</td><td> - </td><td class="header">พนักงานขับรถ</td><td><?php echo $driver; ?></td>
       </tr>
       <tr>
-        <td class="header">ผู้ขอใช้รถ</td><td>นาย ขอยืม หน่อย</td><td class="header">โทรศัพท์</td><td>0864564555</td>
+        <td class="header">ผู้ขอใช้รถ</td><td><?php echo $applicant; ?></td><td class="header">โทรศัพท์</td><td><?php echo $tel; ?></td>
       </tr>
     </table>
   </body>
